fa516b05587c86fdd1153930136b8ffef235a5ce
Jorge Albert <jalbertbos@uoc.edu>
2022-10-26 17:16:33 +0200

    Modificado Cliente_Estandar y Cliente_Premium

--- src/modelo/Cliente_Estandar.java
+++ src/modelo/Cliente_Estandar.java
@@ -1,5 +1,24 @@
 package modelo;
 
 public class Cliente_Estandar extends Cliente{
+        private int numCliente;
 
+        public Cliente_Estandar(int numCliente, String nif, String nombre, String domicilio, String email) {
+            super(nif, nombre, domicilio, email);
+            this.numCliente = numCliente;
+
+        }
+
+        public int getNumCliente() {
+            return numCliente;
+        }
+
+        public void setNumCliente(int numCliente) {
+            this.numCliente = numCliente;
+        }
+
+        @Override
+        public String toString() {
+            return "Cliente_Estandar{" + super.toString() + "numCliente=" + numCliente + '}';
+        }
 }
--- src/modelo/Cliente_Premium.java
+++ src/modelo/Cliente_Premium.java
@@ -1,5 +1,22 @@
 package modelo;
 
 public class Cliente_Premium extends Cliente{
+        private float descuento;
 
+        public Cliente_Premium(float descuento, String nif, String nombre, String domicilio, String email ) {
+            super(nif, nombre, domicilio, email);
+            this.descuento = descuento;
+        }
+
+        public float getDescuento() {
+            return descuento;
+        }
+
+        public void setDescuento(float descuento) {
+            this.descuento = descuento;
+        }
+
+        @Override
+        public String toString() {
+            return "Cliente_Premium(" + super.toString() + "descuento=" + descuento + '}';
 }

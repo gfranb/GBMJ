18a705a29353933067f8468ab0223e7b9fc983a7
gfranb <103999749+gfranb@users.noreply.github.com>
2022-10-29 16:56:52 +0200

    Error arregaldo

--- src/controlador/OnlineStore.java
+++ src/controlador/OnlineStore.java
@@ -1,9 +1,5 @@
 package controlador;
 
-<<<<<<< HEAD
-public class OnlineStore {
-}
-=======
 import vista.GestionOS;
 
 public class OnlineStore {
@@ -13,4 +9,3 @@ public class OnlineStore {
     }
 
 }
->>>>>>> gianfranco

9e3234e47690328b8dc18d293c93560b0e4e5570
gfranb <103999749+gfranb@users.noreply.github.com>
2022-11-01 20:53:40 +0100

    Proyecto Listo

--- src/test/java/controlador/ControladorTest.java
+++ src/test/java/controlador/ControladorTest.java
@@ -9,28 +9,15 @@ class ControladorTest {
 
     @Test
     void anadirArticulo() {
+        Controlador controlador = new Controlador();
+        assertEquals(true, controlador.anadirArticulo("122", "Mesa", (float) 10.22, 6, 20));
 
     }
 
     @Test
-    void mostrarArticulo() {
-    }
-
-    @Test
-    @DisplayName("Añadir cliente")
     void crearCliente() {
+        Controlador controlador = new Controlador();
+        assertEquals(true, controlador.crearCliente("78", "jose@gamil.com", "mayor 3 Valencia", "45678934D", '1'));
 
-    }
-
-    @Test
-    void mostrarCliente() {
-    }
-
-    @Test
-    void showCatalogo() {
-    }
-
-    @Test
-    void cargar() {
     }
 }
